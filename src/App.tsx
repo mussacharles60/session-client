82107249340efdd8eb39eb586f84ee95e3bef32c
Mussa Charles <mussacharles60@gmail.com>
2021-09-25 22:14:58 +0300

    updated code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
+
+import Landing from './components/landing';
+// import Login from './components/login';
+import Home from './components/home';
+import WindowFrame from './components/windowFrame';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="router">
+      <WindowFrame />
+      <Switch>
+        <Route exact path='/' render={({ history}) => <Landing history={history}/> } />
+        {/* <Route path='/login' render={({ history}) => <Login history={history}/> } /> */}
+        <Route path='/home' render={({ history}) => <Home history={history}/> } />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
